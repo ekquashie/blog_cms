64994ca7a304fb8925ff2cb4d3a0886dcd8bd433
Eyram Quashie <eyramkobla.quashie@gmail.com>
2022-02-17 01:33:27 +0000

    added widget to dashboard

--- admin/index.php
+++ admin/index.php
@@ -5,11 +5,6 @@
         <?php
         $conn = openCon();
 
-        if($conn) {
-            echo "";
-        }
-
-        closeCon($conn);
         ?>
 
         <!-- Navigation -->
@@ -29,6 +24,121 @@
                             <small>Author</small>
                         </h1>
 
+
+                        <!-- /.row -->
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-file-text fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <?php
+                                                    $query = "SELECT * FROM `posts`";
+                                                    $get_posts = $conn->query($query);
+                                                    $post_count = $get_posts->num_rows;
+                                                ?>
+                                                <div class='huge'><?php echo $post_count; ?></div>
+                                                <div>Posts</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="posts.php">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">View Details</span>
+                                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="panel panel-green">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-comments fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <?php
+                                                $query = "SELECT * FROM `comments`";
+                                                $get_comments = $conn->query($query);
+                                                $comment_count = $get_comments->num_rows;
+                                                ?>
+                                                <div class='huge'><?php echo $comment_count; ?></div>
+                                                <div>Comments</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="comments.php">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">View Details</span>
+                                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="panel panel-yellow">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-user fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <?php
+                                                $query = "SELECT * FROM `users`";
+                                                $get_users = $conn->query($query);
+                                                $user_count = $get_users->num_rows;
+                                                ?>
+                                                <div class='huge'><?php echo $user_count ?></div>
+                                                <div> Users</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="users.php">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">View Details</span>
+                                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="panel panel-red">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <div class="col-xs-3">
+                                                <i class="fa fa-list fa-5x"></i>
+                                            </div>
+                                            <div class="col-xs-9 text-right">
+                                                <?php
+                                                $query = "SELECT * FROM `categories`";
+                                                $get_categories = $conn->query($query);
+                                                $category_count = $get_categories->num_rows;
+                                                ?>
+                                                <div class='huge'><?php echo $category_count ?></div>
+                                                <div>Categories</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <a href="categories.php">
+                                        <div class="panel-footer">
+                                            <span class="pull-left">View Details</span>
+                                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php closeCon($conn); ?>
+                        <!-- /.row -->
 
                     </div>
                 </div>
